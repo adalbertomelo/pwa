82d9f6014ea80377cb8cbd5709d9e2ca4c827ff7
adalbertocmelo <adalbertocmelo@gmail.com>
2024-05-23 16:59:44 -0300

    commit inicial
    commit inicial

---
+++ appCoordenadas/app.js
@@ -0,0 +1,11 @@
+<script>
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('/sw.js')
+            .then(function(registration) {
+                console.log('Service Worker registrado com sucesso:', registration);
+            })
+            .catch(function(error) {
+                console.log('Falha ao registrar o Service Worker:', error);
+            });
+    }
+</script>
